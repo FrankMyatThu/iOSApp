9ea1e2a209b27a75a1229b9994a669943a035470
Frank Myat Thu <myatthu1986@gmail.com>
2016-08-04 18:44:41 +0800

    Hello World

--- iMusic/iMusic/ViewController.swift
+++ iMusic/iMusic/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblTest: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +22,8 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func txtTest_Action(txtTest: UITextField) {
+        lblTest.text = "Hi \(txtTest.text)"
+    }
 }
 
