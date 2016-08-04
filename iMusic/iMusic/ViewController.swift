9e279de2d6a07231d918edd8bb2465d68a37a828
Frank Myat Thu <myatthu1986@gmail.com>
2016-08-04 17:46:15 +0800

    add blank project

---
+++ iMusic/iMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Myat Thu on 4/8/16.
+//  Copyright © 2016 Ninzimay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
